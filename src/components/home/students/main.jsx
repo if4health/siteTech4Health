db4e8b29f0530d86ed04fac2d1df2a4ad0b58fd3
Vinicius-Borges7 <vinicius.shr@gmail.com>
2023-01-10 15:36:29 -0300

    test

--- src/components/home/students/main.jsx
+++ src/components/home/students/main.jsx
@@ -107,39 +107,42 @@ export default function Main(){
       const url = "http://teste-env.eba-x7kutucd.sa-east-1.elasticbeanstalk.com/";
 
     useEffect(() => {
-        axios.get(`${url}students/data`)
+        // axios.get(`${url}students/data`)
+        fetch("http://teste-env.eba-x7kutucd.sa-east-1.elasticbeanstalk.com/students/data")
         .then((res) => {
-            data = res.data;
-            let list = [];
+            console.log('"""aaa"""')
+            console.log(res)
+    //         data = res.data;
+    //         let list = [];
 
-            data.forEach((e) => {
-                list.push(
-                        <div key={e._id}>
-                            <div className="card card-body mb-3">
-                                <h5 className="card-title">
-                                    <img className="rounded-circle float-left mr-3" src={`${url}images/` + e.mypic} alt={`foto de ${e.name}`} style={{"width":"60px", "height":"60px"}}/>
-                                    {e.name}
-                                </h5>
-                                <h6 className="card-subtitle mb-2 text-muted font-weight-light"> {e.quote}</h6>
-                                <p className="card-text">
-                                    <i aria-hidden="true" className="fa fa-mortar-board text-original-dark-blue"></i>{e.tipo}<br/>
-                                    <i aria-hidden="true" className="fa fa-check text-original-dark-blue"></i>{e.curso}<br/>
-                                    <i aria-hidden="true" className="fa fa-calendar text-original-dark-blue"></i>{e.status}<br/>
-                                </p>
-                                <small className="text-center">
-                                    <div className="btn-group">
-                                        {insertLattes(e.lattes)}
-                                        {insertEmail(e.email)}
-                                        {insertLinkedin(e.linkedin)}
-                                        {insertGithub(e.github)}
-                                    </div>
-                                </small>
-                            </div>
-                        </div>
-                );  
-            });
-            setStudents(list);
-            setCont(cont + 1);
+    //         data.forEach((e) => {
+    //             list.push(
+    //                     <div key={e._id}>
+    //                         <div className="card card-body mb-3">
+    //                             <h5 className="card-title">
+    //                                 <img className="rounded-circle float-left mr-3" src={`${url}images/` + e.mypic} alt={`foto de ${e.name}`} style={{"width":"60px", "height":"60px"}}/>
+    //                                 {e.name}
+    //                             </h5>
+    //                             <h6 className="card-subtitle mb-2 text-muted font-weight-light"> {e.quote}</h6>
+    //                             <p className="card-text">
+    //                                 <i aria-hidden="true" className="fa fa-mortar-board text-original-dark-blue"></i>{e.tipo}<br/>
+    //                                 <i aria-hidden="true" className="fa fa-check text-original-dark-blue"></i>{e.curso}<br/>
+    //                                 <i aria-hidden="true" className="fa fa-calendar text-original-dark-blue"></i>{e.status}<br/>
+    //                             </p>
+    //                             <small className="text-center">
+    //                                 <div className="btn-group">
+    //                                     {insertLattes(e.lattes)}
+    //                                     {insertEmail(e.email)}
+    //                                     {insertLinkedin(e.linkedin)}
+    //                                     {insertGithub(e.github)}
+    //                                 </div>
+    //                             </small>
+    //                         </div>
+    //                     </div>
+    //             );  
+    //         });
+    //         setStudents(list);
+    //         setCont(cont + 1);
         });
     }, []);
 
