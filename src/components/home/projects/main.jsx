f468437c5d82a21316be8e683f86d6dc9533f85e
Vinicius-Borges7 <vinicius.shr@gmail.com>
2023-01-10 13:20:01 -0300

    done

--- src/components/home/projects/main.jsx
+++ src/components/home/projects/main.jsx
@@ -111,51 +111,51 @@ export default function Main(){
 
     const url = "http://teste-env.eba-x7kutucd.sa-east-1.elasticbeanstalk.com/";
 
-    // useEffect(() => {
-    //     axios.get(`${url}projects/data`)
-    //     .then((res) => {
-    //         data = res.data;
-    //         let list = [];
-    //         let key = 0;
+    useEffect(() => {
+        axios.get(`${url}projects/data`)
+        .then((res) => {
+            data = res.data;
+            let list = [];
+            let key = 0;
 
-    //         data.forEach((e) => {
-    //             key += 1;
+            data.forEach((e) => {
+                key += 1;
 
-    //             list.push(
-    //                 <div className="card" key={key}>
-    //                     <div className="card-header">{e.tittle}</div>
-    //                     <img src={`${url}images/` + e.mypic} loading="lazy" className="card-img-top" alt="Projeto 2021"/>
-    //                     <div className="card-body">
-    //                         <p className="card-text">
-    //                             {/* <span className="badge badge-pill badge-primary"><i className="fa fa-check"></i>{e.status}</span> */}
-    //                             <span className={statusColor(e.status)}><i className="fa fa-check"></i>{e.status}</span>
-    //                             <strong> {e.tittle}</strong>
-    //                         </p>
-    //                         <p className="card-text">
-    //                             <strong>Vigência:</strong> {e.vigency}<br/> 
-    //                             <strong>Coordenador:</strong> {e.coordinator}<br/>
-    //                             <strong>Membros:</strong> {membersString(e)}<br/>
-    //                             <strong>Bolsistas:</strong> {scholarsString(e)}
-    //                         </p>
-    //                         <div className="card-text">
-    //                             <span className="text-original-blue font-weight-bold">Aluno do TSI ou ECA:</span>
-    //                             <ul className="no-bullet">
-    //                             <li> <i aria-hidden="true" className="fa fa-check text-original-blue"></i> Desenvolva seu TCC ou sua Pesquisa conosco; </li>
-    //                             <li> <i aria-hidden="true" className="fa fa-check text-original-blue"></i> Temos varias sugestões de trabalhos; </li>
-    //                             </ul>
-    //                         </div><br/><br/>
-    //                         <p className="card-text text-center">
-    //                             <a className="btn btn-success mx-3" href="./ECA" role="button">Alunos ECA</a>
-    //                             <a className="btn btn-success mx-3" href="./TSI" role="button">Alunos TSI</a>
-    //                         </p>
-    //                     </div>
-    //                 </div>
-    //             );  
-    //         });
-    //         setProjects(list);
-    //         setCont(cont + 1);
-    //     });
-    // }, []);
+                list.push(
+                    <div className="card" key={key}>
+                        <div className="card-header">{e.tittle}</div>
+                        <img src={`${url}images/` + e.mypic} loading="lazy" className="card-img-top" alt="Projeto 2021"/>
+                        <div className="card-body">
+                            <p className="card-text">
+                                {/* <span className="badge badge-pill badge-primary"><i className="fa fa-check"></i>{e.status}</span> */}
+                                <span className={statusColor(e.status)}><i className="fa fa-check"></i>{e.status}</span>
+                                <strong> {e.tittle}</strong>
+                            </p>
+                            <p className="card-text">
+                                <strong>Vigência:</strong> {e.vigency}<br/> 
+                                <strong>Coordenador:</strong> {e.coordinator}<br/>
+                                <strong>Membros:</strong> {membersString(e)}<br/>
+                                <strong>Bolsistas:</strong> {scholarsString(e)}
+                            </p>
+                            <div className="card-text">
+                                <span className="text-original-blue font-weight-bold">Aluno do TSI ou ECA:</span>
+                                <ul className="no-bullet">
+                                <li> <i aria-hidden="true" className="fa fa-check text-original-blue"></i> Desenvolva seu TCC ou sua Pesquisa conosco; </li>
+                                <li> <i aria-hidden="true" className="fa fa-check text-original-blue"></i> Temos varias sugestões de trabalhos; </li>
+                                </ul>
+                            </div><br/><br/>
+                            <p className="card-text text-center">
+                                <a className="btn btn-success mx-3" href="./ECA" role="button">Alunos ECA</a>
+                                <a className="btn btn-success mx-3" href="./TSI" role="button">Alunos TSI</a>
+                            </p>
+                        </div>
+                    </div>
+                );  
+            });
+            setProjects(list);
+            setCont(cont + 1);
+        });
+    }, []);
 
     return(
         <section className="mt-3">
