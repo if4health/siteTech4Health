7d08e207c88b5b732660efc9d71289cd0a3bbeab
Vinicius-Borges7 <vinicius.shr@gmail.com>
2023-01-10 13:11:55 -0300

    test

--- src/components/home/students/main.jsx
+++ src/components/home/students/main.jsx
@@ -105,9 +105,10 @@ export default function Main(){
       }
 
       const url = "http://teste-env.eba-x7kutucd.sa-east-1.elasticbeanstalk.com/";
+      const uurrll = "http://teste-env.eba-x7kutucd.sa-east-1.elasticbeanstalk.com/students/data";
 
     useEffect(() => {
-        axios.get(`${url}students/data`)
+        axios.get(uurrll)
         .then((res) => {
             data = res.data;
             let list = [];
