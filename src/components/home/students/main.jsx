de687964f7c57fe8317633199710f2b0d05feadb
Vinicius-Borges7 <vinicius.shr@gmail.com>
2023-01-11 13:29:38 -0300

    .

--- src/components/home/students/main.jsx
+++ src/components/home/students/main.jsx
@@ -111,7 +111,7 @@ export default function Main(){
         fetch("http://teste-env.eba-x7kutucd.sa-east-1.elasticbeanstalk.com/students/data")
         .then((res) => {
             // console.log('"""aaa"""')
-            console.log(res);
+            console.log(res.json());
     //         data = res.data;
     //         let list = [];
 
